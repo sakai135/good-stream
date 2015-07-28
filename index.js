4c4980ba7b4b36dcea85833d8608592917b50e29
sakai135 <sakai135@users.noreply.github.com>
2015-07-28 17:16:08 -0400

    transform to json

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 var Squeeze = require('good-squeeze').Squeeze;
+var SafeJson = require('good-squeeze').SafeJson;
 
 function GoodReporterStream(events, config) {
 	this.squeeze = Squeeze(events);
 	this.writestream = config.stream;
+	this.transform = config.transform || SafeJson()
 }
 
 GoodReporterStream.prototype.init = function (readstream, emitter, callback) {
-	readstream.pipe(this.squeeze).pipe(this.writestream);
+	readstream.pipe(this.squeeze).pipe(this.transform).pipe(this.writestream);
 	callback();
 }
 
